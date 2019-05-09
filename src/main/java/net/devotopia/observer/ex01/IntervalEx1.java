8f96bfa8bbbc374e116688e92fa6514e6d475360
Aiden <hello@devtopia.net>
2019-05-09 22:07:11 +0900

    스프링 리액티브 프로그래밍 (3)

--- src/main/java/net/devotopia/observer/ex01/IntervalEx1.java
+++ src/main/java/net/devotopia/observer/ex01/IntervalEx1.java
@@ -6,30 +6,73 @@ import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class IntervalEx1 {
     private static final Logger logger = LoggerFactory.getLogger(IntervalEx1.class.getName());
 
     public static void main(String[] args) {
         Publisher<Integer> pub = sub -> sub.onSubscribe(new Subscription() {
+            int no = 0;
+            boolean cancelled = false;
+
             @Override
             public void request(long l) {
                 logger.debug("request()");
 
-                sub.onNext(1);
-                sub.onNext(2);
-                sub.onNext(3);
-                sub.onNext(4);
-                sub.onNext(5);
-                sub.onComplete();
+                ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+                executorService.scheduleAtFixedRate(() -> {
+                    if (cancelled) {
+                        executorService.shutdown();
+                        return;
+                    }
+
+                    sub.onNext(this.no++);
+                }, 0, 300, TimeUnit.MILLISECONDS);
             }
 
             @Override
             public void cancel() {
-
+                this.cancelled = true;
             }
         });
 
-        pub.subscribe(new Subscriber<Integer>() {
+        Publisher<Integer> takePub = sub -> {
+            pub.subscribe(new Subscriber<Integer>() {
+                int count = 0;
+                Subscription subc;
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    sub.onSubscribe(s);
+                    this.subc = s;
+                }
+
+                @Override
+                public void onNext(Integer i) {
+                    sub.onNext(i);
+
+                    if (count++ >= 5) {
+                        this.subc.cancel();
+                    }
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    sub.onError(t);
+                }
+
+                @Override
+                public void onComplete() {
+                    sub.onComplete();
+                }
+            });
+        };
+
+        takePub.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 logger.debug("onSubscribe");
